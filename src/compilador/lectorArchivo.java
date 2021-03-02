9c54b2221d3deb2ab9b1a39b11d47d9257cd12ce
FernandoPerea <ferperea0@gmail.com>
2021-03-02 09:33:50 -0600

    Fer

--- src/compilador/lectorArchivo.java
+++ src/compilador/lectorArchivo.java
@@ -15,7 +15,7 @@ public class lectorArchivo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+   int fers;
     }
     
 }
