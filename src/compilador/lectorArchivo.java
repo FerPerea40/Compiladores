1a592164ca2c33ad64b6dfc315db10b4bd6f1252
FernandoPerea <ferperea0@gmail.com>
2021-03-02 10:04:17 -0600

    a medias

--- src/compilador/lectorArchivo.java
+++ src/compilador/lectorArchivo.java
@@ -5,18 +5,75 @@
  */
 package compilador;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Dell
  */
 public class lectorArchivo {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    //Diseñar un programa que permita leer un archivo txt que contenga
+    //caracteres alfanuméricos. Implementar un método que retorne el carácter en el que va en el archivo.
+    //En otro método imprimir a pantalla los caracteres que arroja la 
+    //clase anterior (Dependiendo de cuantas veces se mande llamar el primer método serían los caracteres que mostraría a pantalla).
+    
+     public static void leerDatos() throws IOException{
+    // ventana para abrir el txt
+    
+     String texto, aux;
+     ArrayList<String> lista = new ArrayList();
+        try {
+            //llamamos el metodo que permite cargar la ventana
+            JFileChooser file = new JFileChooser();
+            file.showOpenDialog(file);
+           // FileReader fr = new FileReader("C:\\Users\\carli\\Desktop\\iris.data"); 
+            //abrimos el archivo seleccionado
+            File abre = file.getSelectedFile();
+
+            //recorremos el archivo y lo leemos
+            if (abre != null) {
+                FileReader archivos = new FileReader(abre);
+                BufferedReader lee = new BufferedReader(archivos);
+
+                while ((aux = lee.readLine()) != null) {
+                    texto = aux;
+                    lista.add(texto);
+                    lista.split("");
+                    for(int i=0;i<lista.size();i++){
+
+}
+                }
+                lee.close();
+                System.out.println(lista.size());
+                
+for(int i=0;i<lista.size();i++){
+
+}
+                         
+            }
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, ex + ""
+                    + "\nNo se ha encontrado el archivo",
+                    "ADVERTENCIA!!!", JOptionPane.WARNING_MESSAGE);
+        }
+       
+     }
+
+
+    public static void main(String[] args) throws IOException {
    int fers;
    int carliwis;
+   
+   lectorArchivo.leerDatos();
     }
     
 }
